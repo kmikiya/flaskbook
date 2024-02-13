6f0980c835f53dc53ed0b2e09035befe9596e902
Mikiya <k.mikiya97@gmail.com>
2024-02-13 20:55:04 +0900

    削除機能、共通部分統合

--- apps/app.py
+++ apps/app.py
@@ -3,23 +3,20 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
+from apps.config import config
+import os
 
 # SQLAlchemy をインスタンス化
 db = SQLAlchemy()
 csrf = CSRFProtect()
+config_key = os.getenv("FLASK_APP_CONFIG_KEY")
 
 
+# config_keyは環境変数から取得
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_mapping(
-        SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
-        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        # SQLをコンソールログに出力
-        SQLALCHEMY_ECHO=True,
-        WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f",
-    )
+    app.config.from_object(config[config_key])
 
     # インスタンス初期化⇒アプリケーションが起動される前にデータベースの接続設定やテーブルの作成などの初期化作業ができる
     db.init_app(app)
@@ -30,4 +27,8 @@ def create_app():
     from apps.crud import views as crud_views
 
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
+    # 認証機能
+    from apps.auth import views as auth_views
+
+    app.register_blueprint(auth_views.auth, url_prefix="/auth")
     return app
--- apps/crud/views.py
+++ apps/crud/views.py
@@ -28,6 +28,7 @@ def sql():
     return "コンソールログを確認して下さい"
 
 
+# 新規作成画面
 @crud.route("/users/new", methods=["GET", "POST"])
 def create_user():
     form = UserForm()
@@ -41,3 +42,34 @@ def create_user():
         db.session.commit()
         return redirect(url_for("crud.users"))
     return render_template("crud/create.html", form=form)
+
+
+# 一覧画面
+@crud.route("/users")
+def users():
+    form = UserForm()
+    users = User.query.all()
+    # index.htmlに取得したユーザ情報をもっていく
+    return render_template("crud/index.html", users=users, form=form)
+
+
+@crud.route("/users/<user_id>", methods=["GET", "POST"])
+def edit_user(user_id):
+    form = UserForm()
+    user = User.query.filter_by(id=user_id).first()
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("crud.users"))
+    return render_template("crud/edit.html", user=user, form=form)
+
+
+@crud.route("/users/<user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for("crud.users"))
